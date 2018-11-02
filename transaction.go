c581f7b0aeb9cc825db1c705064b6cb72744b825
KevNet <kevnet@protonmail.com>
2018-11-02 17:41:10 +0100

    Removed Writer interface implementation
    There won't be a case where you want to write a single transaction -- for now

--- transaction.go
+++ transaction.go
@@ -1,17 +1,9 @@
 package main
 
-import "io"
-import "encoding/json"
-
 type Transaction struct {
 	Date            string `json:"date"`
 	Reason          string `json:"reason"`
 	ItemName        string `json:"itemName"`
 	ItemDescription string `json:"itemDescription"`
 	Amount          int    `json:"amount"`
-}
-
-func (t *Transaction) Write(w io.Writer) {
-	b, _ := json.Marshal(*t)
-	w.Write(b)
 }
