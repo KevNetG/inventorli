64d0a011fb7e5f52b83967c86e6943831c7b62ec
KevNet <kevnet@protonmail.com>
2018-11-02 16:07:46 +0100

    Implemented Writer interface and changed Date from type time.Time to string
    The Date property of a Transaction will be a string formatted as yyyy/mm/dd. This information will suffice

--- transaction.go
+++ transaction.go
@@ -1,11 +1,17 @@
 package main
 
-import "time"
+import "io"
+import "encoding/json"
 
 type Transaction struct {
-	Date            time.Time `json:"date"`
-	Reason          string 	  `json:"reason"`
-	ItemName        string    `json:"itemName"`
-	ItemDescription string    `json:"itemDescription"`
-	Amount          int       `json:"amount"`
+	Date            string `json:"date"`
+	Reason          string `json:"reason"`
+	ItemName        string `json:"itemName"`
+	ItemDescription string `json:"itemDescription"`
+	Amount          int    `json:"amount"`
+}
+
+func (t *Transaction) Write(w io.Writer) {
+	b, _ := json.Marshal(*t)
+	w.Write(b)
 }
