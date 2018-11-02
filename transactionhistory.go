1f48d31476a65595bb98e1a326b011d2bb4bbf30
KevNet <kevnet@protonmail.com>
2018-11-02 17:43:06 +0100

    Implemented Writer interface

--- transactionhistory.go
+++ transactionhistory.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+    "io"
+    "encoding/json"
+)
+
 type TransactionHistory struct {
     Transactions []Transaction `json:"transactions"`
+}
+
+func (t *TransactionHistory) Write(w io.Writer) {
+    b, _ := json.Marshal(*t)
+    w.Write(b)
 }
