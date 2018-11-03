57501dfdddf9edadfde74885215b242ce77664c5
KevNet <kevnet@protonmail.com>
2018-11-03 18:24:17 +0100

    inventorli list commands shows description

--- cmd/list.go
+++ cmd/list.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"inventorli/inventory"
+	"math"
 )
 
 var cmdList = &cobra.Command{
@@ -27,6 +28,14 @@ func listRun(cmd *cobra.Command, args []string) {
 
 	for i, item := range inv.Items {
 		fmt.Println(fmt.Sprintf("[%d] %s", i, item.Name))
+		fmt.Printf("   ")
+
+		for j := 0; j < int(math.Log10(float64(i+1))+1); j++ {
+			fmt.Print(" ")
+		}
+
+		fmt.Printf(fmt.Sprintf("%s", item.Description))
+		fmt.Println()
 	}
 }
 
