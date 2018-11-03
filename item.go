d5e35e470c574095896094ba1d1ce656e99d1c1c
KevNet <kevnet@protonmail.com>
2018-11-03 13:01:34 +0100

    Removed left over files

--- item.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type Item struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-}
--- transaction.go
+++
@@ -1,8 +0,0 @@
-package main
-
-type Transaction struct {
-	Date   string `json:"date"`
-	Reason string `json:"reason"`
-	Item   Item   `json:"item"`
-	Amount int    `json:"amount"`
-}
