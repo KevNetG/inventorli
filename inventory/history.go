41f6cc1fba56bbce85b8902d7ae447a583caf163
KevNet <kevnet@protonmail.com>
2018-11-03 17:16:07 +0100

    Added method to directly write to a history file

--- inventory/history.go
+++ inventory/history.go
@@ -10,8 +10,8 @@ type History struct {
 	Transactions []Transaction `json:"transactions"`
 }
 
-func (t *History) Write(w io.Writer) error {
-	b, err := json.Marshal(*t)
+func (h *History) Write(w io.Writer) error {
+	b, err := json.Marshal(*h)
 	if err != nil {
 		return err
 	}
@@ -20,14 +20,31 @@ func (t *History) Write(w io.Writer) error {
 	return err
 }
 
-func (t *History) Read(r io.Reader, n int64) {
-	b := make([]byte, n)
-	r.Read(b)
-	json.Unmarshal(b, t)
+// Truncates the file before writing
+func (h *History) WriteFile(path string) error {
+	f, err := os.OpenFile(path, os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// Set cursor to write at the beginning of the file
+	f.Truncate(0)
+	f.Seek(0, 0)
+
+	err = h.Write(f)
+
+	return err
 }
 
-func (t *History) ReadFile(path string) error {
-	f, err := os.OpenFile(path, os.O_RDWR, 0644)
+func (h *History) Read(r io.Reader, n int64) {
+	b := make([]byte, n)
+	r.Read(b)
+	json.Unmarshal(b, h)
+}
+
+func (h *History) ReadFile(path string) error {
+	f, err := os.OpenFile(path, os.O_RDONLY, 0644)
 	if err != nil {
 		return err
 	}
@@ -38,7 +55,6 @@ func (t *History) ReadFile(path string) error {
 		return err
 	}
 
-	h := History{}
 	h.Read(f, stat.Size())
 
 	return nil
