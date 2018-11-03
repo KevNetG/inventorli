7becd9dbbdcc9fd35d23e97ed515062db619d2cd
KevNet <kevnet@protonmail.com>
2018-11-03 17:31:55 +0100

    Added amount flag for remove command

--- cmd/root.go
+++ cmd/root.go
@@ -7,10 +7,12 @@ import (
 )
 
 var file string
+var reason string
+var amount int
 
 var rootCmd = &cobra.Command{
-	Use:   "invemtory",
-	Short: "Inventory is a tool for managing and tracking your stuff",
+	Use:   "inventorli",
+	Short: "inventorli is a tool for managing and tracking your stuff",
 	Long:  `TODO: Long description`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Print("lol")
